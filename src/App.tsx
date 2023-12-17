aea46afef40f9f5cd2942e83cb54e09d59ae2883
zgolmohamadi <zgh402@gmail.com>
2023-12-17 10:04:20 +0330

    fix: remove defaults

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-  
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(
-            position => {
-                console.log(position.coords);
-            },
-            error => {
-                console.log("Error: ", error)
-            },{ enableHighAccuracy: true });
-}
+      (position) => {
+        console.log(position.coords);
+      },
+      (error) => {
+        console.log("Error: ", error);
+      },
+      { enableHighAccuracy: true }
+    );
+  }
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(success, error);
   } else {
     console.log("Geolocation not supported");
   }
-  
-  function success(position:any) {
+
+  function success(position: any) {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
     console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
   }
-  
+
   function error() {
     console.log("Unable to retrieve your location");
   }
 
   return (
     <>
-      <div>
-       
-     </div>
+      <div></div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
